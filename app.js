9ca1fec816dd2d185ac337ae23d05e162612425d
ilhom003 <abdulahatov03gmail.com>
2023-02-02 22:32:54 +0500

    array methods

---
+++ app.js
@@ -0,0 +1,63 @@
+// let arr = [1, 2, { name: 'Ilhom' }, function () { }]
+
+// console.log(arr[0]);
+// console.log(arr[arr.length-1]);
+// console.log(arr.at(1));
+
+// let ar = new Array(5,1);
+// let ar = [1,2,3];
+
+// ar[1] = 1
+// console.log(ar);
+// console.log(ar.length);
+
+// let ar1 = [1, 2, 3, 4];
+// let ar2 = [4, 5, 6]
+// console.log(ar1.toString());
+// console.log(ar1.join(' '));
+// console.log(ar1.concat(ar2));
+
+// console.log(typeof []);
+// console.log(Array.isArray([]));
+
+// let ar = []
+// console.log(ar == 0);
+
+// let user = ['Ilhom', 'Islom', 'Rustam', 'Sulton', 'Mirkomil'];
+
+// for (let i = 0; i < user.length; i++) {
+//     console.log(i);    
+// }
+
+// for (const value of user) {
+//     console.log(value);
+// }
+// for (const index in user) {
+//     console.log(index);
+// } --> index chiqarib beradi
+
+
+// let ar = [1, 2, 3];
+// ar.push(4)
+// ar.pop()
+// ar.unshift(0,2)
+// ar.shift()
+// console.log(ar);
+
+
+// let ar = [1,2,4,5];
+// ar.splice(1,2,'wba',3,7)
+// ar.splice(2,0,3)
+// ar.slice(0,2)
+// console.log(ar);
+
+
+// let ar = [1,2,4,5,4];
+// console.log(ar.indexOf(4));
+// console.log(ar.lastIndexOf(4)); 
+// ar.reverse();
+// console.log(ar.includes(5));
+
+
+
+
